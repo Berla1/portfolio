b19bb8dd9969ad2dd08aefee612245c5067960f6
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2025-01-29 20:03:38 -0300

    adicionando mais projetos e atualizando currículo

--- src/components/Projetos/index.tsx
+++ src/components/Projetos/index.tsx
@@ -1,13 +1,48 @@
 import CardProjeto from "./CardProjeto";
 import capaPortfolio from "../../assets/imagens-projetos/capa-portfolio.svg";
+import capaCarrinho from "../../assets/imagens-projetos/capa-carrinho-iot.svg";
+import capaCardapio from "../..//assets/imagens-projetos/capa-cardapio.svg";
+import CMais from "../../assets/tecnologias/logo-c++.svg";
 import TypeScript from "../../assets/tecnologias/logo-typescript.svg";
 import TailwindCSS from "../../assets/tecnologias/logo-tailwind.svg";
+import Python from "../../assets/tecnologias/logo-python.svg";
+import Java from '../../assets/tecnologias/logo-java.svg';
+import MySQL from '../../assets/tecnologias/logo-mysql.svg';
 
 const Projetos = () => {
   return (
-    <section className="flex flex-col h-full md:p-25 scroll-m-14 py-10" id="projetos">
+    <section
+      className="flex flex-col h-full md:p-25 scroll-m-14 py-10"
+      id="projetos"
+    >
       <h1 className="text-white py-10 text-4xl md:text-6xl">Projetos</h1>
       <div className="flex flex-wrap gap-10">
+        <CardProjeto
+          fotoProjeto={capaCarrinho}
+          nomeProjeto="Carrinho controlado por gestos"
+          descricaoProjeto="Este projeto implementa um sistema de controle baseado em gestos da mão usando Mediapipe, OpenCV, e MQTT para controlar motores e um servo via comandos remotos. Com uma câmera, gestos são detectados em tempo real para realizar ações como mover um motor ou ajustar o ângulo de um servo. Usando Python e C++ para a comunicação entre os dispositivos. Realizado como projeto acadêmico."
+          githubBotao={false}
+          githubLink="https://github.com/Berla1/hand-gesture-control"
+          youtubeBotao={false}
+          youtubeLink="https://www.youtube.com/watch?v=0BRyTk86wu4"
+          tecnologia1={Python}
+          tecnologia2={CMais}
+        />
+
+        <CardProjeto
+          fotoProjeto={capaCardapio}
+          nomeProjeto="Cardápio fullstack"
+          descricaoProjeto="O Cardápio Fullstack é uma aplicação web que permite aos usuários visualizar e gerenciar um cardápio digital. Construido utilizando TypeScript, Java com Spring Boot e MySQL para persistência de dados."
+          githubBotao={false}
+          githubLink="https://github.com/Berla1/cardapio-fullstack"
+          youtubeBotao={false}
+          youtubeLink="https://www.youtube.com/watch?v=ClySm8FVcf0"
+          tecnologia1={TypeScript}
+          tecnologia2={Java}
+          tecnologia3={MySQL}
+        />
+
+
         <CardProjeto
           fotoProjeto={capaPortfolio}
           nomeProjeto="Meu portfólio pessoal"
