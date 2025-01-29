ccd9cd05803c74478480b57ed20b2babf8386151
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2025-01-29 17:55:00 -0300

    cursor pointer no botão

--- src/components/Home/RedeBotao/index.tsx
+++ src/components/Home/RedeBotao/index.tsx
@@ -5,7 +5,7 @@ interface RedeBotaoProps {
 
 const RedeBotao = ({ Icone, Texto }: RedeBotaoProps) => {
   return (
-    <button className="group/button relative inline-flex items-center justify-center overflow-hidden rounded-md  backdrop-blur-lg px-6 py-3 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-102 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20">
+    <button className="cursor-pointer group/button relative inline-flex items-center justify-center overflow-hidden rounded-md  backdrop-blur-lg px-6 py-3 text-base font-semibold text-white transition-all duration-300 ease-in-out hover:scale-102 hover:shadow-xl hover:shadow-gray-600/50 border border-white/20">
       <span className="flex items-center justify-center gap-2 md:text-2xl">
         {Icone} {Texto}
       </span>
