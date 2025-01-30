3f702491021214ec907a6a7ef377bbb0e0b04f57
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2025-01-30 01:20:06 -0300

    corrigindo z-index do header para mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="flex md:z-21 bg-[#111] fixed pl-1 md:justify-evenly w-full md:pl-10">
+    <header className="flex z-21 bg-[#111] fixed pl-1 md:justify-evenly w-full md:pl-10">
       <nav>
         <ul className="flex gap-5 py-6 md:py-8 text-white">
           <a href="#home"><li className="links cursor-pointer">Home</li></a>
